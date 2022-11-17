a13a93a85c1494d2f90397b8a6be7bb2e6b2e679
Saravut Nakglom <snakglom@gmail.com>
2022-11-18 03:06:18 +0700

    fix: Html dir

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn handle_connection(mut stream: TcpStream) {
         .collect();
 
     let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("./src/index.html").unwrap();
+    let contents = fs::read_to_string("index.html").unwrap();
     let length = contents.len();
 
     let response =
